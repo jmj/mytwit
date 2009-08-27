3b0f86c9e629d49a1cc022b0cc80da4b02c1ea84
jmj <jeremyj@letifer.org>
2009-08-27 00:37:11 -0500

    condev.py - thread starter for ipy to enable GUI dev in the ipy console     start a console and import condev.  Taken from winforms.py in the     IronPython dist ui.py - moved all of the UI code here, wrapped inside an Application() ui.py.forms - start of Forms() impl of the UI.  I hate Forms(), why     can't mono implement WPF
    bg_thread.py - fix a missing ','
    main.py - comment out all of the UI code (moved to ui.py).  Inst a ui.app() and run it.

---
+++ ui.py
@@ -0,0 +1,57 @@
+
+import clr
+
+clr.AddReference('PresentationCore')
+clr.AddReference('PresentationFramework')
+
+from System.IO import File
+from System.Windows.Markup import XamlReader
+from System.Windows import Controls as WinControls
+from System.Windows import Media as WinMedia
+from System.Windows import Thickness
+from System.Windows import TextWrapping
+
+from System.Windows import Application
+
+import bg_thread
+
+
+class app(Application):
+    winelem = dict()
+
+    def run(self):
+        self.Run(self.mainwin)
+
+
+    def __init__(self, uifile='ui.xaml'):
+        self._ui_file = File.OpenRead(uifile)
+        self.mainwin = XamlReader.Load(self._ui_file)
+        self.Waddle(self.mainwin.Content, self.winelem)
+
+        self.client = bg_thread.MT_API(username='jmj42', password='ickysonf')
+
+        self.mainwin.Loaded += lambda _,__: self.client.GetPublicTimeline(
+            self.status_cb)
+
+
+    ## Stolen from http://docs.google.com/Doc?id=dd59dk39_23ckv9qkfs
+    ## dumps the ui heiarchy into a dict()
+    def Waddle(self, c, d):
+        s = str(c.__class__)
+        if str(c).find("System.Windows.Controls.")>-1 and hasattr(c,"Name") and c.Name.Length>0:
+            ControlType = s[s.find("'")+1:s.rfind("'")]
+            if not d.has_key(ControlType):
+                d[ControlType] = {}
+            d[ControlType][c.Name] = c
+        if hasattr(c,"Children"):
+            for cc in c.Children:
+                self.Waddle(cc, d)
+        elif hasattr(c,"Child"):
+            self.Waddle(c.Child, d)
+        elif hasattr(c,"Content"):
+            self.Waddle(c.Content, d)
+
+    def status_cb(app, bgworker, evArgs):
+        print evArgs.Cancelled
+        print evArgs.Error
+        print evArgs.Result
